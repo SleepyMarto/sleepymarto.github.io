af73de02473360040cbea1a325036ab2c6c7034c
SleepyMarto <153505847+SleepyMarto@users.noreply.github.com>
2024-03-30 22:54:10 +0200

    js crap. might use later

---
+++ siteresources/javascript/socials.js
@@ -0,0 +1,20 @@
+const quotes = [
+    "2 + 2 = 5",
+    "I don't know",
+    "Press refresh",
+    "No_U",
+    "lol",
+    "SlepeMartoe'sAnIdiot",
+    "SleepeMartoe'sAutictis",
+    "lol",
+    //"Need me a Dommy Mommy",
+    "Good luck!",
+    "Hi",
+    "Hello",
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    //"Need me a Domme",
+    "Yes"
+];
+    
+const random = Math.floor(Math.random() * quotes.length);
+document.write(quotes[random]);
